b28e9d4685e6eaaced818ec3d107c06963c03c78
servervaivaicomputer <server.vaivaicomputer@gmail.com>
2026-09-10 06:30:26 +0600

    Update index.js

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
+const pageRoutes = require('./pages');
 
 router.use('/api', apiRoutes);
+router.use('/page', pageRoutes);
 
 module.exports = router;
